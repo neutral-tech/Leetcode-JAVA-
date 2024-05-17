1f1d3d0ab192d8602efb86bce4ff66ff0ac2dfca
neutral-tech <neutraltech.firoj@gmail.com>
2024-05-17 10:28:09 +0530

    POTD MAY 17

---
+++ Medium/p_1325.java
@@ -0,0 +1,9 @@
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if(root==null) return null;
+        root.left=removeLeafNodes(root.left,target);
+        root.right=removeLeafNodes(root.right,target);
+        if(root.right==null && root.left==null && root.val==target) return null;
+        return root;
+    }
+}
