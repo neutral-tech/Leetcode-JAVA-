d7ed2a0f612bd10571c798fcc0db122f09890e8f
neutral-tech <neutraltech.firoj@gmail.com>
2024-05-20 13:53:15 +0530

    Optimized

--- Easy/p_70.java
+++ Easy/p_70.java
@@ -1,3 +1,19 @@
+
+//Optimized solution
+class Solution {
+    public int climbStairs(int n) {
+        if(n<1) return 0;
+        int first=1,second=1,i,now=1;
+        for(i=1;i<n;i++){
+            now=first+second;
+            first=second;
+            second=now;
+        }
+        return now;
+    }
+}
+
+
 class Solution {
     int[] memo;
     public int climbStairs(int n) {
